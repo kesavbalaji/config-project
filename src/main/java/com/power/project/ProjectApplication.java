6095f9c5cc76c21643116a66d01c04277c4669c3
kesav <kesavbalaji28@gmail.com>
2024-07-08 17:18:51 +0530

    Inital commit config project

---
+++ src/main/java/com/power/project/ProjectApplication.java
@@ -0,0 +1,15 @@
+package com.power.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectApplication.class, args);
+	}
+
+}
